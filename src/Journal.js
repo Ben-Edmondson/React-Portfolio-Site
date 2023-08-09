356ef242402d68aa9f5fe03e5f95a24b24ab8c70
Ben Edmondson <ben.edmondson99@gmail.com>
2023-08-09 11:51:06 +0100

    Removed unneeded stuff.

--- src/Journal.js
+++ src/Journal.js
@@ -1,9 +1,7 @@
 import React from "react";
-import MonthlyEntry from "./MonthlyEntry";
 import Calendar from "./Calendar";
 
 function Journal() {
-    const month = "August";
     return(
     <div>
         <header className="App-header">
