f89d678c976b8981b1f1533ea99f4b85f7685a5f
Ben Edmondson <ben.edmondson99@gmail.com>
2023-08-09 14:08:46 +0100

    Updated file structure

---
+++ src/Home/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Homepage from './Homepage';
+import Projects from './Projects';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import About from './About';
+import Journal from '../Journal/Journal';
+
+
+function Navbar() {
+  return (
+    <Router>
+        <nav className='nav'>
+        <Link className='site-title text' to="/">Ben Edmondson</Link>
+            <ul>
+                <li className='active'>
+                <Link className='navtext' to="/">Home</Link>
+                </li>
+                <li>
+                <Link className='navtext' to="/about">About</Link>
+                </li>
+                <li>
+                <Link className='navtext' to="/projects">Projects</Link>
+                </li>
+                <li>
+                  <Link className='navtext' to="/journal">Journal</Link>
+                </li>
+            </ul>
+        </nav>
+
+        <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/journal" element={<Journal />} />
+        </Routes>
+    </Router>
+  );
+};
+
+export default Navbar;
---
+++ src/Journal/Journal.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Calendar from "./Calendar";
+
+function Journal() {
+    return(
+    <div>
+        <header className="App-header">
+        <h1>Journal</h1>
+        <Calendar/>
+        </header>
+      </div>
+    );
+};
+
+export default Journal;
