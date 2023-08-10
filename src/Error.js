8dc04d9a505f6cc09e120a8215d58dd2ddf6cf56
Ben Edmondson <ben.edmondson99@gmail.com>
2023-08-10 15:13:39 +0100

    Lol

---
+++ src/Error.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Error() {
+    return(
+    <div>
+        <header className="App-header">
+        <h1>Broke</h1>
+        </header>
+      </div>
+    );
+};
+
+export default Error;
